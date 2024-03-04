255857404aa13f28616141d5a359a6d73126e090
sky <3374614481@qq.com>
2024-03-04 13:50:51 +0800

    update

--- txn/src/main.rs
+++ txn/src/main.rs
@@ -1,25 +1,40 @@
 use std::vec;
 
 use anyhow::Result;
-use databend_driver::new_connection;
+use databend_driver::Client;
 use util::ConnectionExt;
 mod util;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     //export DATABEND_DSN="databend://root:@localhost:8000/?sslmode=disable"
     let dsn = std::env::var("DATABEND_DSN").unwrap();
 
-    let c1 = new_connection(&dsn)?;
-    let c2 = new_connection(&dsn)?;
+    let client = Client::new(dsn);
+    let c1 = client.get_conn().await.unwrap();
+    let c2 = client.get_conn().await.unwrap();
 
     let select_t = "SELECT * FROM t ORDER BY c;";
 
     c1.exec("CREATE OR REPLACE TABLE t(c int);").await?;
 
     c1.begin().await?;
-    c1.exec("INSERT INTO t VALUES(2);").await?;
-    c1.assert_query(select_t, vec![(2,)]).await;
-
+    c1.exec("INSERT INTO t VALUES(1);").await?;
+    c1.assert_query(select_t, vec![(1,)]).await;
     c2.assert_query::<(i32,)>(select_t, vec![]).await;
+
+    c2.begin().await?;
+    c2.exec("INSERT INTO t VALUES(2);").await?;
+    c1.assert_query(select_t, vec![(1,)]).await;
+    c2.assert_query(select_t, vec![(2,)]).await;
+
+    c2.commit().await?;
+    c1.assert_query(select_t, vec![(1,)]).await;
+    c2.assert_query(select_t, vec![(2,)]).await;
+
+    let result = c1.commit().await;
+    assert!(result.is_err());
+    c1.assert_query(select_t, vec![(2,)]).await;
+    // c2.assert_query(select_t, vec![(2,)]).await;
     Ok(())
 }
--- txn/src/util.rs
+++ txn/src/util.rs
@@ -8,7 +8,7 @@ pub trait ConnectionExt: Connection {
     async fn assert_query<T>(&self, sql: &str, expected: Vec<T>)
     where
         T: TryFrom<databend_driver::Row> + 'static + Debug + PartialEq,
-        <T as TryFrom<databend_driver::Row>>::Error: std::error::Error + Send + Sync,
+        <T as TryFrom<databend_driver::Row>>::Error: Debug,
     {
         let result: Vec<T> = self.exec_query(sql).await.unwrap();
         assert_eq!(result, expected);
@@ -17,12 +17,12 @@ pub trait ConnectionExt: Connection {
     async fn exec_query<T>(&self, sql: &str) -> Result<Vec<T>>
     where
         T: TryFrom<databend_driver::Row> + 'static,
-        <T as TryFrom<databend_driver::Row>>::Error: std::error::Error + Send + Sync,
+        <T as TryFrom<databend_driver::Row>>::Error: Debug,
     {
         let mut rows = self.query_iter(sql).await?;
         let mut res = vec![];
         while let Some(r) = rows.next().await {
-            let row: T = r.unwrap().try_into()?;
+            let row: T = r.unwrap().try_into().unwrap();
             res.push(row);
         }
         Ok(res)
@@ -30,6 +30,16 @@ pub trait ConnectionExt: Connection {
 
     async fn begin(&self) -> Result<()> {
         self.exec("BEGIN").await?;
+        Ok(())
+    }
+
+    async fn commit(&self) -> Result<()> {
+        self.exec("COMMIT").await?;
+        Ok(())
+    }
+
+    async fn rollback(&self) -> Result<()> {
+        self.exec("ROLLBACK").await?;
         Ok(())
     }
 }
