20435035ff5b1666e29fd6095821f4aeb63df082
sky <3374614481@qq.com>
2024-03-08 22:07:33 +0800

    update sql

--- task/task.sql
+++ task/task.sql
@@ -6,38 +6,38 @@ AS BEGIN
         BEGIN;
         MERGE INTO json_table_flag_1 AS target
         USING (SELECT * FROM json_table_stream) source
-        ON source. source_flag = 1 AND target.id = source.id
+        ON source.v:source_flag = 1 AND target.id = source.v:id
         WHEN MATCHED THEN 
-                UPDATE SET target.id = source.id,target.create_at = source.create_at
+                UPDATE SET target.id = source.v:id,target.create_at = source.v:create_at
         WHEN NOT MATCHED THEN
-                INSERT (id,create_at) VALUES (source.id,source.create_at)
+                INSERT (id,create_at) VALUES (source.v:id,source.v:create_at)
     ;
 
         MERGE INTO json_table_flag_2 AS target
             USING (SELECT * FROM json_table_stream) source
-        ON source. source_flag = 2 AND target.id = source.id
+        ON source.v:source_flag = 2 AND target.id = source.v:id
         WHEN MATCHED THEN 
-                UPDATE SET target.id = source.id,target.create_at = source.create_at
+                UPDATE SET target.id = source.v:id,target.create_at = source.v:create_at
         WHEN NOT MATCHED THEN
-                INSERT (id,create_at) VALUES (source.id,source.create_at)
+                INSERT (id,create_at) VALUES (source.v:id,source.v:create_at)
     ;
 
         MERGE INTO json_table_flag_3 AS target
         USING (SELECT * FROM json_table_stream) source
-        ON source. source_flag = 3 AND target.id = source.id
+        ON source.v:source_flag = 3 AND target.id = source.v:id
         WHEN MATCHED THEN 
-                UPDATE SET target.id = source.id,target.create_at = source.create_at
+                UPDATE SET target.id = source.v:id,target.create_at = source.v:create_at
         WHEN NOT MATCHED THEN
-                INSERT (id,create_at) VALUES (source.id,source.create_at)
+                INSERT (id,create_at) VALUES (source.v:id,source.v:create_at)
     ;
 
         MERGE INTO json_table_flag_4 AS target
         USING (SELECT * FROM json_table_stream) source
-        ON source. source_flag = 4 AND target.id = source.id
+        ON source.v:source_flag = 4 AND target.id = source.v:id
         WHEN MATCHED THEN 
-                UPDATE SET target.id = source.id,target.create_at = source.create_at
+                UPDATE SET target.id = source.v:id,target.create_at = source.v:create_at
         WHEN NOT MATCHED THEN
-                INSERT (id,create_at) VALUES (source.id,source.create_at)
+                INSERT (id,create_at) VALUES (source.v:id,source.v:create_at)
     ;
         COMMIT
     ;
