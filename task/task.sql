139a2c59c7efea7f232d68cfd903fd66041cfe88
sky <3374614481@qq.com>
2024-03-08 21:15:14 +0800

    update sql

--- task/task.sql
+++ task/task.sql
@@ -8,36 +8,36 @@ AS BEGIN
         USING (SELECT * FROM json_table_flag_1_stream) source
         ON source. source_flag = 1 AND target.id = source.id
         WHEN MATCHED THEN 
-                UPDATE [...]
+                UPDATE SET target.id = source.id,target.create_at = source.create_at
         WHEN NOT MATCHED THEN
-                INSERT [...]
+                INSERT (id,create_at) VALUES (source.id,source.create_at)
     ;
 
         MERGE INTO json_table_flag_2 AS target
             USING (SELECT * FROM json_table_flag_2_stream) source
         ON source. source_flag = 2 AND target.id = source.id
         WHEN MATCHED THEN 
-                UPDATE [...]
+                UPDATE SET target.id = source.id,target.create_at = source.create_at
         WHEN NOT MATCHED THEN
-                INSERT [...]
+                INSERT (id,create_at) VALUES (source.id,source.create_at)
     ;
 
         MERGE INTO json_table_flag_3 AS target
         USING (SELECT * FROM json_table_flag_3_stream) source
         ON source. source_flag = 3 AND target.id = source.id
         WHEN MATCHED THEN 
-                UPDATE [...]
+                UPDATE SET target.id = source.id,target.create_at = source.create_at
         WHEN NOT MATCHED THEN
-                INSERT [...]
+                INSERT (id,create_at) VALUES (source.id,source.create_at)
     ;
 
         MERGE INTO json_table_flag_4 AS target
         USING (SELECT * FROM json_table_flag_4_stream) source
         ON source. source_flag = 4 AND target.id = source.id
         WHEN MATCHED THEN 
-                UPDATE [...]
+                UPDATE SET target.id = source.id,target.create_at = source.create_at
         WHEN NOT MATCHED THEN
-                INSERT [...]
+                INSERT (id,create_at) VALUES (source.id,source.create_at)
     ;
         COMMIT
     ;
