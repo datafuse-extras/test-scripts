4a1fccc8364a1cf4798f24b7a9247a1172889bed
dantengsky <dantengsky@gmail.com>
2025-06-16 14:04:44 +0800

    Add new test case auto_vacuum

--- the-suite/src/main.rs
+++ the-suite/src/main.rs
@@ -5,11 +5,13 @@ use clap::Subcommand;
 use env_logger::Env;
 use log::info;
 
+mod auto_vacuum;
 mod change_tracking;
 mod explict_txn;
 mod multi_table_insert;
 mod util;
 
+use auto_vacuum::Args as AutoVacuumArgs;
 use change_tracking::Args as ChangeTrackingArgs;
 use change_tracking::ChangeTrackingSuite;
 
@@ -25,6 +27,7 @@ enum Commands {
     ChangeTracking(ChangeTrackingArgs),
     ExplicitTxn,
     MultiTableInsert,
+    AutoVacuum(AutoVacuumArgs),
 }
 
 #[tokio::main]
@@ -41,5 +44,6 @@ async fn main() -> Result<()> {
         Commands::ChangeTracking(cmd_args) => ChangeTrackingSuite::run(cmd_args, dsn).await,
         Commands::ExplicitTxn => explict_txn::run(dsn).await,
         Commands::MultiTableInsert => multi_table_insert::run(dsn).await,
+        Commands::AutoVacuum(cmd_args) => auto_vacuum::run(cmd_args, dsn).await,
     }
 }
